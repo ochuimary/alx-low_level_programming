97810721ba568ce462cfc11963a2935704adefe1
ochuimary <117898623+ochuimary@users.noreply.github.com>
2022-12-10 17:32:50 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,43 +1,22 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 
-
-
 /**
+ * main - returns an alphabet in reverse order
  *
- *  * main - returns an alphabet in reverse order
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
+	int ch = 'z';
 
-		int ch = 'z';
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
 
-
-
-			while (ch >= 'a')
-
-					{
-
-								putchar(ch);
-
-										ch--;
-
-											}
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 }

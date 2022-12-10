2eaf617c92f821d038ab8635d7eba509373b4559
ochuimary <117898623+ochuimary@users.noreply.github.com>
2022-12-10 17:22:20 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,26 @@
-mary 
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return single integer from 0-10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
+Footer
+© 2022 GitHub, Inc.

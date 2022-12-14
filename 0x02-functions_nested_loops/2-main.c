1595a7e8f541fbfd9d9fcd9fb9058e53bb0ed292
ochuimary <117898623+ochuimary@users.noreply.github.com>
2022-12-14 10:41:56 +0100

    Update 2-main.c

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,21 +1,12 @@
 #include "main.h"
 
-
-
 /**
+ * main - check the code.
  *
- *  * main - check the code.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
-
-	    print_alphabet_x10();
-
-	   return (0);i}
+    print_alphabet_x10();
+    return (0);
+}

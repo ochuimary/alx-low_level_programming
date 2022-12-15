a219bd48ad7fc993a5f107206e89df1ae094691a
ochuimary <117898623+ochuimary@users.noreply.github.com>
2022-12-15 16:06:58 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,31 +1,16 @@
-#include "main.h"
-
-
+#include <unistd.h>
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- *  * print_numbers - prints number
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  *
- *   */
+ */
 
-
-
-void print_numbers(void)
-
+int _putchar(char c)
 {
-
-		int i;
-
-
-
-			for (i = 48; i < 58; i++)
-
-					{
-
-								_putchar(i);
-
-									}
-
-				_putchar('\n');
-
+	
+	return (write(1, &c, 1));
 }

56c309f274d8633fb3335b1e307d54309df05253
ochuimary <117898623+ochuimary@users.noreply.github.com>
2022-12-15 16:22:26 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,35 +1,18 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_line - prints line
- *
- *   * @n: param
- *
- *    */
-
-
+ * print_line - prints line
+ * @n: param
+ */
 
 void print_line(int n)
-
 {
+	int i = 1;
 
-		int i = 1;
-
-
-
-			while (i <=  n)
-
-					{
-
-								_putchar(95);
-
-										i++;
-
-											}
-
-				_putchar('\n');
-
+	while (i <=  n)
+	{
+		_putchar(95);
+		i++;
+	}
+	_putchar('\n');
 }

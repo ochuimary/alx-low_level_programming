5498cc9f765fdd5a108d8a30be36c9d6bfa2a722
ochuimary <117898623+ochuimary@users.noreply.github.com>
2022-12-15 15:59:42 +0100

    Update 2-main.c

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -1,13 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * mul - multiplies 2 nums
- * @a: first param
- * @b: second param
- * Return: something
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-int mul(int a, int b)
+int main(void)
 {
-	return (a * b);
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
+    return (0);
 }

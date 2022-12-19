13feb30aae8a24c397c696665831ec91a9035064
ochuimary <117898623+ochuimary@users.noreply.github.com>
2022-12-19 10:29:11 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,49 +1,25 @@
 #include <stdio.h>
-
 #include "main.h"
-
 /**
- *
- *  * rev_string - This function prints a string in reverse order
- *
- *   * @s: pointer parameter
- *
- *    */
-
-
+ * rev_string - This function prints a string in reverse order
+ * @s: pointer parameter
+ */
 
 void rev_string(char *s)
-
 {
+	int len, i, half;
+	char temp;
 
-		int len, i, half;
+	for (len = 0; s[len] != '\0'; len++)
+	;
+	i = 0;
+	half = len / 2;
 
-			char temp;
-
-
-
-				for (len = 0; s[len] != '\0'; len++)
-
-						;
-
-					i = 0;
-
-						half = len / 2;
-
-
-
-							while (half--)
-
-									{
-
-												temp = s[len - i - 1];
-
-														s[len - i - 1] = s[i];
-
-																s[i] = temp;
-
-																		i++;
-
-																			}
-
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
 }

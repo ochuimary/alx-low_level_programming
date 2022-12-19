18ee04189e0afe9f840713f5ee343c6bb61b7e7f
ochuimary <117898623+ochuimary@users.noreply.github.com>
2022-12-19 10:02:37 +0100

    Update 1-main.c

--- 0x05-pointers_arrays_strings/1-main.c
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -1,39 +1,17 @@
+#include <stdio.h>
 #include "main.h"
 
-#include <stdio.h>
-
-
-
 /**
- *
- *  * main - check the code
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+ * swap_int - a function that swaps the values of two integers.
+ * @a: first value to be provided
+ * @b: Second parameter to be provided
+ * int c: addressed the swapped value will be sent to
+ * Return: Always 0 (Success)
+ */
 
-int main(void)
-
+void swap_int(int *a, int *b)
 {
-
-	    int a;
-
-	        int b;
-
-
-
-		    a = 98;
-
-		        b = 42;
-
-			    printf("a=%d, b=%d\n", a, b);
-
-			        swap_int(&a, &b);
-
-				    printf("a=%d, b=%d\n", a, b);
-
-				        return (0);
-
+	int c = *a;
+	*a = *b;
+	*b = c;
 }

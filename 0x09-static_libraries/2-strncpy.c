73ce4c827c5922697ba9d6c3353aeeab287f7f18
ochuimary <117898623+ochuimary@users.noreply.github.com>
2023-01-06 17:35:38 +0100

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,59 +1,30 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- *  * _strncpy - copies a string
- *
- *   * @dest: Pointer to a character that will be changed
- *
- *    * @src: Pointer to a character that will be changed
- *
- *     * @n: value
- *
- *      * Return: dest
- *
- *       */
-
-
+ * _strncpy - copies a string
+ * @dest: Pointer to a character that will be changed
+ * @src: Pointer to a character that will be changed
+ * @n: value
+ * Return: dest
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 
-
-
 {
+	int i;
 
-		int i;
+	i = 0;
 
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
 
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
-			i = 0;
-
-
-
-				while (i < n && src[i] != '\0')
-
-						{
-
-									dest[i] = src[i];
-
-											i++;
-
-												}
-
-
-
-					while (i < n)
-
-							{
-
-										dest[i] = '\0';
-
-												i++;
-
-													}
-
-
-
-						return (dest);
-
+	return (dest);
 }

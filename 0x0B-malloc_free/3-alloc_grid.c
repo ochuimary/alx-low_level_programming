da2a19daefb2b105a678dfb6227f6f01f3d6d767
ochuimary <117898623+ochuimary@users.noreply.github.com>
2023-01-10 11:36:47 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,85 +1,43 @@
 #include "main.h"
-
 /**
- *
- *  * alloc_grid - allocates a grid, make space and free space
- *
- *   * @width: takes in width of grid
- *
- *    * @height: height of grid
- *
- *     * Return: grid with freed spaces
- *
- *      */
-
-
+ * alloc_grid - allocates a grid, make space and free space
+ * @width: takes in width of grid
+ * @height: height of grid
+ * Return: grid with freed spaces
+ */
 
 int **alloc_grid(int width, int height)
-
 {
+	/*Declaring variables*/
+	int **grid;
+	int i, j;
 
-		/*Declaring variables*/
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
 
-		int **grid;
+	grid = malloc(sizeof(int *) * height); /*malloc*/
 
-			int i, j;
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
 
-
-
-				if (width <= 0 || height <= 0)
-
-						{
-
-									return (NULL);
-
-										}
-
-
-
-					grid = malloc(sizeof(int *) * height); /*malloc*/
-
-
-
-						if (grid == NULL)
-
-								{
-
-											return (NULL);
-
-												}
-
-
-
-							for (i = 0; i < height; i++)
-
-									{
-
-												grid[i] = malloc(sizeof(int) * width);
-
-														if (grid[i] == NULL)
-
-																	{
-
-																					for (i = i - 1; i >= 0; i--)
-
-																									{
-
-																														free(grid[i]);
-
-																																	}
-
-																							free(grid);
-
-																									return (NULL);
-
-																											}
-
-															}
-
-								for (i = 0; j < width; j++)
-
-										grid[i][j] = 0;
-
-									return (grid);
-
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+			{
+				free(grid[i]);
+			}
+		free(grid);
+		return (NULL);
+		}
+	}
+	for (i = 0; j < width; j++)
+	grid[i][j] = 0;
+	return (grid);
 }
